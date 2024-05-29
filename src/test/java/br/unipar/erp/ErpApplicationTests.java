ed201eb9a7f8cc01e4e41b03ac90516bd454c294
raissabonfim <raissa230306@edu.unipar.br>
2024-05-28 22:29:50 -0300

    COMMIT

--- src/test/java/br/unipar/erp/ErpApplicationTests.java
+++ src/test/java/br/unipar/erp/ErpApplicationTests.java
@@ -1,13 +1,28 @@
 package br.unipar.erp;
 
+import br.unipar.erp.data.entity.Usuario;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ErpApplicationTests {
+import java.time.LocalDate;
 
-	@Test
-	void contextLoads() {
+@SpringBootTest
+class ErpApplicationTests implements CommandLineRunner {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ErpApplicationTests.class, args);
+
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Usuario usuario = new Usuario();
+		usuario.setNome("Spring JPA");
+		usuario.setSenha("123456");
+		usuario.setUsuario("springjpa");
+		usuario.setDataNascimento(LocalDate.of(1993,6,20));
+	}
 }
--- src/test/java/br/unipar/erp/data/entity/Usuario.java
+++ src/test/java/br/unipar/erp/data/entity/Usuario.java
@@ -1,23 +1,39 @@
 package br.unipar.erp.data.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
-
-@Table
+@Getter
+@Setter
+@Table(name="usuario")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
 
     @Id
     private Integer codigo;
 
+
+    private String nome;
+
+    @Column(name="username")
     public String usuario;
 
+   @Column(name="password")
     private String senha;
 
-    
+    @Column(name= "nascimento")
     private LocalDate dataNascimento;
+
+
+
 }
